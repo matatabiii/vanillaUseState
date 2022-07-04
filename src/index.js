5a2c01476213afa44e1ff00f0e6aa4a67c379d0b
Keishiro Kawaguchi <27771489+matatabiii@users.noreply.github.com>
2022-07-04 23:08:56 +0900

    Vanilla JSでReactのuseState風を再現

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const appElement = document.getElementById("app");
+
+let count = 0;
+
+appElement.insertAdjacentHTML(
+  "beforeend",
+  `
+  <div className="App">
+    <h1 id="counter">0</h1>
+    <button id="count-down">カウント -1</button>
+    <button id="count-reset">リセット</button>
+    <button id="count-up">カウント +1</button>
+  </div>
+  `
+);
+
+const counter = document.getElementById("counter");
+const countUpBtn = document.getElementById("count-up");
+const countDownBtn = document.getElementById("count-down");
+const countResetBtn = document.getElementById("count-reset");
+
+const setCount = (value) => {
+  count = value;
+  counter.textContent = count;
+};
+
+const countUp = (event) => {
+  event.preventDefault();
+  setCount(count + 1);
+};
+
+const countDown = (event) => {
+  event.preventDefault();
+  setCount(count - 1);
+};
+
+const countReset = (event) => {
+  event.preventDefault();
+  setCount(0);
+};
+
+countUpBtn.addEventListener("click", countUp);
+countDownBtn.addEventListener("click", countDown);
+countResetBtn.addEventListener("click", countReset);
