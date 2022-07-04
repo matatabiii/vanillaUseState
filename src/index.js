42df24c51d8d5fb12d656b1d134e13186850fbdd
Keishiro Kawaguchi <27771489+matatabiii@users.noreply.github.com>
2022-07-04 23:34:52 +0900

    countが10の時にテキストをredにする処理を追加

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ appElement.insertAdjacentHTML(
     <button id="count-down">カウント -1</button>
     <button id="count-reset">リセット</button>
     <button id="count-up">カウント +1</button>
+    <div id="text"></div>
   </div>
   `
 );
@@ -20,20 +21,36 @@ const counter = document.getElementById("counter");
 const countUpBtn = document.getElementById("count-up");
 const countDownBtn = document.getElementById("count-down");
 const countResetBtn = document.getElementById("count-reset");
+const text = document.getElementById("text");
+
+const changeText = () => {
+  const value = count === 10 ? "10だよ" : "10以外だよ";
+  text.textContent = value;
+  if (count === 10) {
+    text.style.color = "red";
+  } else if (text.style.color === "red") {
+    text.style.color = "";
+  }
+};
+
+changeText();
 
 const setCount = (value) => {
   count = value;
   counter.textContent = count;
+  changeText();
 };
 
 const countUp = (event) => {
   event.preventDefault();
   setCount(count + 1);
+  changeText();
 };
 
 const countDown = (event) => {
   event.preventDefault();
   setCount(count - 1);
+  changeText();
 };
 
 const countReset = (event) => {
